5a3774e8bd7532bac1f1e4969779e5090c370024
shayan2079 <shayan.sh20079@gmail.com>
2023-07-28 23:53:14 +0330

    feat: Added search page

---
+++ app/coin-info/SearchCoins.tsx
@@ -0,0 +1,198 @@
+'use client';
+import {
+  Button,
+  CircularProgress,
+  Grid,
+  InputAdornment,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@mui/material';
+import React, { useEffect, useState, useTransition } from 'react';
+import { CoinInfo } from './page';
+import Image from 'next/image';
+import { usePathname, useRouter } from 'next/navigation';
+import dynamic from 'next/dynamic';
+import Link from 'next/link';
+
+interface Props {
+  coinsInfo: CoinInfo[];
+  page: undefined | number;
+  initialSearch: undefined | string;
+}
+
+const CustomTableCell = ({
+  text,
+  color,
+  align,
+}: {
+  text: string;
+  color?: string;
+  align?: 'left';
+}) => {
+  return (
+    <TableCell align={align}>
+      <Typography textAlign={align} color={color ?? 'inherit'}>
+        {text}
+      </Typography>
+    </TableCell>
+  );
+};
+
+// components/MyChart.js contains the recharts chart
+const TextField = dynamic(
+  () => import('@mui/material').then((MyComponent) => MyComponent.TextField),
+  { ssr: false }
+);
+
+const SearchCoins : React.FC<Props> = ({ coinsInfo, page, initialSearch }) => {
+  const [search, setSearch] = useState(initialSearch ?? '');
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const addToLocalStorage = (search) => {
+    if (!search || coinsInfo.length === 0) return;
+    const favouriteCoins = JSON.parse(localStorage.getItem('favourite-coins'));
+    const isAdded = favouriteCoins.some((item) => item === search);
+    if (Array.isArray(favouriteCoins) && !isAdded) {
+      favouriteCoins.push(search);
+      if (favouriteCoins.length > 3) {
+        favouriteCoins.shift();
+      }
+      localStorage.setItem('favourite-coins', JSON.stringify(favouriteCoins));
+    } else if (!isAdded) {
+      localStorage.setItem('favourite-coins', JSON.stringify([search]));
+    }
+  };
+
+  const handleChangeRoute = (page, search) => {
+    let newQuery = '';
+    if (!!page && !!search) newQuery = `?page=${page}&search=${search}`;
+    else if (!!page) newQuery = `?page=${page}`;
+    else if (!!search) newQuery = `?search=${search}`;
+    if (!!search) newQuery += ``;
+    startTransition(() => router.push(pathname + newQuery));
+  };
+
+  useEffect(() => {
+    const timeout = setTimeout(() => handleChangeRoute(1, search), 1000);
+    return () => clearTimeout(timeout);
+  }, [search]);
+
+  useEffect(() => {
+    addToLocalStorage(search);
+  }, [JSON.stringify(coinsInfo)]);
+
+  return (
+    <Grid
+      component={'section' as any}
+      container
+      direction={'column'}
+      bgcolor="background.paper"
+      p={3}
+      pb={7}
+    >
+      <Typography variant={'h3'} sx={{ mb: 3 }}>
+        crypto currency prices by market cap
+      </Typography>
+      <TextField
+        fullWidth
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        InputProps={{
+          endAdornment: isPending && (
+            <InputAdornment position="start">
+              <CircularProgress />
+            </InputAdornment>
+          ),
+        }}
+      />
+      {coinsInfo.length === 0 ? (
+        <Typography color={'error.main'}>no results found</Typography>
+      ) : (
+        <>
+          {' '}
+          <TableContainer component={Paper as any} sx={{ mt: 3 }}>
+            <Table
+              sx={{ minWidth: 650, bgcolor: 'background.paper' }}
+              aria-label="coins info"
+            >
+              <TableHead sx={{ bgcolor: 'warning.dark' }}>
+                <TableRow>
+                  <CustomTableCell align={'left'} text={'coin'} />
+                  <CustomTableCell text={'price'} />
+                  <CustomTableCell text={'24h change'} />
+                  <CustomTableCell text={'market cap'} />
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {coinsInfo.map((coin) => (
+                  <TableRow
+                    key={coin.id}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  >
+                    <TableCell>
+                      <Link
+                        href={`/coin-info/${coin.id}`}
+                        style={{ color: 'inherit', textDecoration: 'none' }}
+                      >
+                        <Grid container>
+                          <Image
+                            src={coin.img}
+                            alt={coin.name}
+                            height={35}
+                            width={35}
+                          />
+                          <Grid item pl={1}>
+                            <Typography variant={'body2'}>
+                              {coin.name}
+                            </Typography>
+                            <Typography variant={'caption'}>
+                              {coin.symbol}
+                            </Typography>
+                          </Grid>
+                        </Grid>
+                      </Link>
+                    </TableCell>
+                    <CustomTableCell text={'$' + coin.price} />
+                    <CustomTableCell
+                      color={
+                        coin.priceChange > 0 ? 'success.dark' : 'error.dark'
+                      }
+                      text={coin.priceChange}
+                    />
+                    <CustomTableCell text={coin.marketCap} />
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <Grid
+            item
+            container
+            justifyContent={'space-between'}
+            direction={'row'}
+          >
+            {page && page > 1 && (
+              <Button onClick={() => handleChangeRoute(page - 1, search)}>
+                prev page
+              </Button>
+            )}
+            <Grid item />
+            <Button onClick={() => handleChangeRoute((page || 1) + 1, search)}>
+              next page
+            </Button>
+          </Grid>
+        </>
+      )}
+    </Grid>
+  );
+};
+
+export default SearchCoins;
---
+++ app/coin-info/page.tsx
@@ -0,0 +1,90 @@
+import { Box, Grid, Typography } from '@mui/material';
+import Image from 'next/image';
+import SearchCoins from './SearchCoins';
+
+export interface CoinInfo {
+  id: string;
+  name: string;
+  symbol: string;
+  img: string;
+  marketCap: string;
+  priceChange: string;
+  price: string;
+}
+
+export const metadata = {
+  title: 'currency search page',
+};
+
+export const revalidate = 1500;
+
+const CoinsListPage = async ({
+  searchParams: { page, search },
+}: {
+  searchParams: { page: string | undefined; search: string | undefined };
+}) => {
+  const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd${
+    search ? `&ids=${search}` : ''
+  }&order=market_cap_desc&per_page=5&page=${
+    page || 1
+  }&sparkline=false&locale=en`;
+  const data = await fetch(url).then((res) => res.json());
+
+  const coinsInfo: CoinInfo[] = data.map((info) => ({
+    id: info.id,
+    name: info.name,
+    symbol: info.symbol,
+    img: info.image,
+    marketCap: info.market_cap,
+    priceChange: info.price_change_24h,
+    price: info.current_price,
+  }));
+
+  return (
+    <Box
+      sx={{
+        height: 'calc(100vh - 64px)',
+        width: '100vw',
+        position: 'relative',
+      }}
+    >
+      <Box
+        sx={{
+          position: 'absolute',
+          top: 0,
+          right: 0,
+          width: '100%',
+          height: '100%',
+          zIndex: 1,
+        }}
+      >
+        <Image src={'/banner.jpg'} alt={'coins info'} fill sizes="100vw" />
+      </Box>
+      <Grid
+        container
+        direction={'column'}
+        alignItems={'center'}
+        zIndex={2}
+        position={'relative'}
+        justifyContent={'space-between'}
+        height={1}
+      >
+        <Grid item>
+          <Typography variant={'h1'} mt={4} mb={2} color={'common.white'}>
+            search coin
+          </Typography>
+          <Typography variant={'h4'} color={'common.white'}>
+            get information from here
+          </Typography>
+        </Grid>
+        <SearchCoins
+          coinsInfo={coinsInfo}
+          page={page ? +page : undefined}
+          initialSearch={search}
+        />
+      </Grid>
+    </Box>
+  );
+};
+
+export default CoinsListPage;
